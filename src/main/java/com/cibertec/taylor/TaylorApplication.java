b3bc65809bfdf06672fc6ce88e2b1d2a5284dafe
Raquel <rscm024@gmail.com>
2025-06-15 23:51:36 -0500

    Inicio del proyecto TS

---
+++ src/main/java/com/cibertec/taylor/TaylorApplication.java
@@ -0,0 +1,13 @@
+package com.cibertec.taylor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaylorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaylorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cibertec/taylor/TaylorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cibertec.taylor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaylorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
